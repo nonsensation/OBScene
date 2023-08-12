24a18ad811e5bb8265806ba4f65b5fd8a499ad91
Elias Beyer <beyer.elias@gmail.com>
2023-08-12 18:06:23 +0200

    display dexie images

--- src/routes/(app)/match/+page.svelte
+++ src/routes/(app)/match/+page.svelte
@@ -1,1 +1,28 @@
+<style lang="postcss">
+    img {
+        width: 3em;
+        height: auto;
+    }
+</style>
+
 <h1>Under Construction</h1>
+
+<ul>
+    {#if $teams}
+        {#each $teams as team (team.id)}
+            <li>
+                <span>
+                    {team.name} ({team.shortName})
+                </span>
+                <img src={URL.createObjectURL(team.logo)} alt="" />
+            </li>
+        {/each}
+    {/if}
+</ul>
+
+<script>
+    import { liveQuery } from "dexie";
+    import { db } from "$lib/database/dexie-db";
+
+    let teams = liveQuery(() => db.teams.toArray());
+</script>
